7cb672d32a9d1c2fef7bc47655d952e2ee3f18a3
Ithorion <antoine_giraud2@hotmail.fr>
2014-06-19 15:37:36 +0200

    Retour normal button data-toggle

--- templates/header.php
+++ templates/header.php
@@ -25,6 +25,12 @@
     <div role="navigation" class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
           <a class="navbar-brand" href=""><?= $title ?></a>
         </div>
         <div class="navbar-collapse collapse">
